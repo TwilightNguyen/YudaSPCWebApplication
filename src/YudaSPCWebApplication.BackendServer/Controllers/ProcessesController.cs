57aa1807954b3581a7967873ec83c977bc490669
TwilightNguyen <caotri.0696@gmail.com>
2026-01-15 11:10:50 +0000

    Create Process Api

---
+++ src/YudaSPCWebApplication.BackendServer/Controllers/ProcessesController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YudaSPCWebApplication.BackendServer.Data;
+using YudaSPCWebApplication.ViewModels.System;
+
+namespace YudaSPCWebApplication.BackendServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = "Bearer")]
+    public class ProcessesController(ApplicationDbContext context) : ControllerBase
+    {
+        // Controller code will go here
+
+        public readonly ApplicationDbContext _context = context;
+
+        /// <summary>
+        /// Url: /api/processes/
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = _context.Processes.ToList();
+
+            if (result == null || result.Count == 0) return NotFound("No processes found.");
+
+            var processVms = result.Select(process => new ProcessVm
+            {
+                Id = process.IntID,
+                Name = process.StrProcessName ?? string.Empty,
+                AreaId = process.IntAreaID
+            }).ToList();
+
+            return Ok(processVms);
+        }
+
+        /// <summary>
+        /// Url: /api/proccess/{Id}
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        /// 
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            var process = _context.Processes.FirstOrDefault(r => r.IntID == Id);
+
+            if (process == null)
+            {
+                return NotFound("Process not found.");
+            }
+
+            var processVm = new ProcessVm
+            {
+                Id = process.IntID,
+                Name = process.StrProcessName ?? string.Empty,
+                AreaId = process.IntAreaID
+            };
+            return Ok(processVm);
+        }
+
+        /// <summary>
+        /// Url: /api/proccess/{Id}
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        /// 
+        [HttpGet("ByAreaId")]
+        public async Task<IActionResult> ByAreaId(int AreaId)
+        {
+            var result = _context.Processes.Where(x => x.IntAreaID == AreaId).ToList();
+
+            if (result == null || result.Count == 0) return NotFound("No processes found.");
+
+            var processVms = result.Select(process => new ProcessVm
+            {
+                Id = process.IntID,
+                Name = process.StrProcessName ?? string.Empty,
+                AreaId = process.IntAreaID
+            }).ToList();
+
+            return Ok(processVms);
+        }
+    }
+}
