34588ccf782edc79a479d460283b32b25c8366b6
TwilightNguyen <caotri.0696@gmail.com>
2026-02-12 03:33:11 +0000

    add Shift controller Api

---
+++ src/YudaSPCWebApplication.BackendServer/Controllers/ShiftsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YudaSPCWebApplication.BackendServer.Data;
+using YudaSPCWebApplication.ViewModels.System;
+
+namespace YudaSPCWebApplication.BackendServer.Controllers
+{
+    public class ShiftsController(ApplicationDbContext context) : ControllerBase
+    {
+        public required ApplicationDbContext _context = context;
+
+        ///<summary>
+        /// Url: /api/shifts
+        /// </summary>
+        /// 
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var shifts = await _context.Shifts.ToListAsync();
+            if(shifts == null || shifts.Count == 0)
+            {
+                return NotFound("No Shift found.");
+            }
+
+            var shiftVms = shifts.Select(shift => new ShiftVm
+            {
+                Id = shift.IntID,
+                Name = shift.StrNameShift,
+                StartTime = shift.DtStartTime,
+                EndTime = shift.DtEndTime,
+            });
+
+            return Ok(shiftVms);
+        }
+
+        ///<summary>
+        /// Url: /api/shifts/{Id}
+        /// </summary>
+        /// 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var shift = _context.Shifts.FirstOrDefault(x => x.IntID == id);
+
+            if (shift == null) {
+                return NotFound("Shift not found.");
+            }
+            var shiftVm = new ShiftVm
+            {
+                Id = shift.IntID,
+                Name = shift.StrNameShift,
+                StartTime = shift.DtStartTime,
+                EndTime = shift.DtEndTime,
+            };
+
+            return Ok(shiftVm);
+        }
+    }
+
+
+}
---
+++ src/YudaSPCWebApplication.ViewModels/System/ShiftVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace YudaSPCWebApplication.ViewModels.System
+{
+    public class ShiftVm
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public TimeSpan? StartTime { get; set; }
+
+        public TimeSpan? EndTime { get; set; }
+    }
+}
